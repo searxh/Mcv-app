490d0a85327c146f039c0624c0810202414b6bf2
Nattakit Pisitsup <search@Nattakits-MacBook-Pro.local>
2022-04-18 19:31:43 +0700

    added twrnc

---
+++ pages/Login.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import tw from 'twrnc'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <View style={tw`bg-blue-500`}>
+      <Text>This is the login page</Text>
+    </View>
+  )
+}
+
+export default Login
