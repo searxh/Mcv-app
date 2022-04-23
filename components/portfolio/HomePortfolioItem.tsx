696f57ce988eb293012464fd5cb320c49504c7eb
Nattakit Pisitsup <search@Nattakits-MacBook-Pro.local>
2022-04-23 19:17:19 +0700

    grades page complete

--- components/portfolio/HomePortfolioItem.tsx
+++ components/portfolio/HomePortfolioItem.tsx
@@ -4,37 +4,58 @@ import tw from 'twrnc'
 import { FontAwesomeIcon as FontAwesomeNative } from '@fortawesome/react-native-fontawesome'
 import { faSquarePollVertical, faShareFromSquare } from '@fortawesome/free-solid-svg-icons'
 
-const HomePortfolioItem = ({ props }:any) => {
+const HomePortfolioItem = ({ props, isTotal, style }:any) => {
     return (
         <View 
-            style={tw`w-[97%] flex-1 justify-between mx-1 flex-row bg-white rounded-md shadow-md p-2 py-5 mx-auto mb-1`}
+            style={tw`w-[97%] justify-between mx-1 flex-row ${style} 
+            ${!isTotal?'bg-white':'bg-amber-500'} rounded-md shadow-md p-2 py-5 mx-auto mb-1`}
         >
             <View style={tw`mx-1`}>
-                <Text style={tw.style('text-sky-700 text-sm mx-1',{
-                    fontFamily:'noto-sans-medium'
-                })}>
+                <Text style={!isTotal?
+                    tw.style('text-sky-700 text-sm mx-1',{
+                        fontFamily:'noto-sans-medium'
+                    }):
+                    tw.style('text-white text-sm mx-1',{
+                        fontFamily:'noto-sans-medium'
+                    })}
+                >
                     {props.item}
                 </Text>
             </View>
             <View style={tw`mx-1`}>
-                <Text style={tw.style('text-green-600 text-base mx-1',{
-                    fontFamily:'noto-sans-medium'
-                })}>
-                    {props.points}
+                <Text style={!isTotal?
+                    tw.style('text-green-600 text-base mx-1',{
+                        fontFamily:'noto-sans-medium'
+                    }):
+                    tw.style('text-white text-base mx-1',{
+                        fontFamily:'noto-sans-medium'
+                    })}
+                >
+                    {props.points.toFixed(2)}
                 </Text>
             </View>
             <View style={tw`mx-1`}>
-                <Text style={tw.style('text-neutral-700 text-base mx-1',{
-                    fontFamily:'noto-sans-medium'
-                })}>
+                <Text style={!isTotal?
+                    tw.style('text-neutral-700 text-sm mx-1',{
+                        fontFamily:'noto-sans-medium'
+                    }):
+                    tw.style('text-white text-sm mx-1',{
+                        fontFamily:'noto-sans-medium'
+                    })}
+                >
                     / {props.totalpoints}
                 </Text>
             </View>
             <View style={tw`mx-1`}>
-                <Text style={tw.style('text-neutral-700 text-base mx-1',{
-                    fontFamily:'noto-sans-medium'
-                })}>
-                    {props.percent}.00%
+                <Text style={!isTotal?
+                    tw.style('text-neutral-700 text-base mx-1',{
+                        fontFamily:'noto-sans-medium'
+                    }):
+                    tw.style('text-amber-500 text-base mx-1',{
+                        fontFamily:'noto-sans-medium'
+                    })}
+                >
+                    {props.percent.toFixed(2)}%
                 </Text>
             </View>
             <View style={tw`flex-row`}>
@@ -44,7 +65,7 @@ const HomePortfolioItem = ({ props }:any) => {
                     <FontAwesomeNative
                         icon={faSquarePollVertical}
                         size={25}
-                        style={tw`text-sky-600 mx-1`}
+                        style={tw`${!isTotal?'text-sky-600':'text-white'} mx-1`}
                     />
                 </TouchableOpacity>
                 <TouchableOpacity
@@ -53,7 +74,7 @@ const HomePortfolioItem = ({ props }:any) => {
                     <FontAwesomeNative
                         icon={faShareFromSquare}
                         size={25}
-                        style={tw`text-sky-600 mx-1`}
+                        style={tw`${!isTotal?'text-sky-600':'text-white'} mx-1`}
                     />
                 </TouchableOpacity>
             </View>
--- pages/CourseSections/SecPortfolio.tsx
+++ pages/CourseSections/SecPortfolio.tsx
@@ -1,4 +1,4 @@
-import { View, Text, FlatList, TouchableOpacity } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import tw from 'twrnc'
 import { grades } from '../../data/grades'
@@ -7,9 +7,7 @@ import HomePortfolioItem from '../../components/portfolio/HomePortfolioItem'
 const SecPortfolio = () => {
   return (
     <View style={tw`bg-sky-100 h-full`}>
-      <View 
-            style={tw`w-[97%] justify-between mx-1 flex-row bg-sky-600 rounded-md shadow-md p-1 py-2 mx-auto my-2`}
-        >
+        <View style={tw`w-[97%] justify-between mx-1 flex-row bg-sky-600 rounded-md shadow-md p-1 py-2 mx-auto my-2`}>
           <View style={tw`flex-1 m-auto`}>
               <Text style={tw.style('text-center text-white text-sm mx-1',{
                   fontFamily:'noto-sans-medium'
@@ -41,24 +39,63 @@ const SecPortfolio = () => {
           <View style={tw`flex-1 m-auto`}>
           </View>
         </View>
-      <FlatList
-          data={grades}
-          keyExtractor={(item: any) => item.id}
-          ListEmptyComponent={
-              <View style={tw`p-10`}>
-                  <Text
-                      style={tw.style('text-xl text-center text-sky-600',{
-                          fontFamily:'noto-sans-medium'
-                      })}
-                  >
-                      There are no assignments
-                  </Text>
-              </View>
-          }
-          renderItem={({ item }) => {
-              return <HomePortfolioItem props={item} />
-          }}
-      />
+        <FlatList
+            data={grades}
+            keyExtractor={(item: any) => item.id}
+            style={tw`flex-1`}
+            ListEmptyComponent={
+                <View style={tw`p-10`}>
+                    <Text
+                        style={tw.style('text-xl text-center text-sky-600',{
+                            fontFamily:'noto-sans-medium'
+                        })}
+                    >
+                        There are no assignments
+                    </Text>
+                </View>
+            }
+            renderItem={({ item, index }) => {
+                if (index===grades.length-1) {
+                    return(
+                        <>
+                            <HomePortfolioItem props={item} isTotal={false} />
+                        </>
+                    )
+                } else {
+                    return <HomePortfolioItem props={item} isTotal={false} />
+                }
+            }}
+        />
+        <View style={tw`flex-1`}>
+            <HomePortfolioItem
+                style=""
+                props={{
+                    item:"Current total",
+                    percent:100,
+                    points:100,
+                    totalpoints:100,
+                }}
+                isTotal={true}
+            />
+            <View style={tw`bg-white shadow-md w-[97%] mx-auto rounded-lg p-3 my-1`}>
+                <Text
+                    style={tw.style('text-base text-sky-600',{
+                        fontFamily:'noto-sans-medium'
+                    })}
+                >
+                    Ranking: NA
+                </Text>
+            </View>
+            <View style={tw`bg-white shadow-md w-[97%] mx-auto rounded-lg p-3 mt-1`}>
+                <Text
+                    style={tw.style('text-base text-sky-600',{
+                        fontFamily:'noto-sans-medium'
+                    })}
+                >
+                    Grades: NA
+                </Text>
+            </View>
+        </View>
     </View>
   )
 }
